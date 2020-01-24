88c8763f9ac28bb5fa3da95dfaa8acec5816a9b6
chris <christoph.myllner@gmx.at>
2020-01-24 23:07:47 +0100

    Add solution and basic web api

---
+++ KmLog.Server/KmLog.Server.WebApi/Controllers/KmLogController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace KmLog.Server.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class KmLogController : ControllerBase
+    {
+        private readonly ILogger<KmLogController> _logger;
+
+        public KmLogController(ILogger<KmLogController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
